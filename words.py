1849e40df2a2512bb1f796ad2d2e362c4c295df4
David O'Callaghan <david.ocallaghan@cs.tcd.ie>
2017-01-10 21:21:56 +0000

    By default don't use dirty words when generating wordlist.js

--- words.py
+++ words.py
@@ -5,15 +5,18 @@ import glob
 from os.path import splitext, basename
 
 words = {}
+USE_DIRTY = False
 
 src = "txt/*"
 dest = "js/wordlist.js"
 
-for file in glob.glob(src):
-    with open(file, 'r') as f:
-        place = splitext( basename(file) )[0]
+for _file in glob.glob(src):
+    if "dirty_" in _file and not USE_DIRTY:
+        continue
+    with open(_file, 'r') as f:
+        place = splitext(basename(_file))[0]
         # using list(set( words )) unique-ifies the words
-        words[place] = list(set( [i.strip() for i in f] ))
+        words[place] = list(set([i.strip() for i in f]))
 
 with open(dest, 'w') as f:
     f.write("var words = ")
